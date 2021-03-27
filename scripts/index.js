adbaf1ed4443affb5b66451a35b82200466b3f08
Ruslan Nagiev <ultraduck100@gmail.com>
2021-03-27 20:53:08 +0300

    refactor: add adaptive carousel layout

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+import { defineCardsAmount } from "./modules/carousel.js";
 import dropdownToggle from "./modules/dropdown-toggle.js";
 import rangeSlider from "./modules/range-slider.js";
 import toggleMenu from "./modules/toggle-menu.js";
@@ -8,3 +9,4 @@ const burger = document.querySelector('.burger');
 toggleMenu(burger, 'active', header);
 dropdownToggle();
 rangeSlider();
+defineCardsAmount();
